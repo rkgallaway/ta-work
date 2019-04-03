71145b42dd55ec00fd15ab80f88ea2d76e42cfa4
rkgallaway <rkgallaway@msn.com>
2019-04-03 15:11:01 -0700

    submission updates

--- 201d47/Richard/201-git-test/js/app.js
+++ 201d47/Richard/201-git-test/js/app.js
@@ -1,76 +1,63 @@
 'use strict'
 //Confirming that the user wants to play the game
-var intro = confirm('Quick get ready want to play guessing game? Only Answer with Y and N');
 var count = 0 //counts the number of times you get an answer correct
-if(intro){
+var intro = confirm('Quick get ready want to play guessing game? Only Answer with Y and N');
+
+if(intro){ 
     //Where did i work Question
-    var myJob = prompt('Did i work in the navy?');
-    myJob === myJob.toUpperCase;
-    console.log('Did i work in the navy?');
-    if(myJob === 'Y'){
+    var myJob = prompt('Did i work in the navy?').toUpperCase();
+    if(myJob === 'Y' || myJob === "YES"){
         alert('Wrong Answer Sorry');
-    } else if(myJob === 'N'){
+    } else if(myJob === 'N'|| myJob === "NO"){
         alert('Good job you guessed right!');
         count++;
     }else{
         alert('Sorry but you didnt follow my instructions please only answer with\
          Y and N.');
-    }
+    } console.log(myJob);
     //What is my age question
-    var myAge = prompt('Is my age 22');
-    myAge === myAge.toUpperCase;
-    console.log('Is my age 22');
-    if(myAge === 'Y'){
+    var myAge = prompt('Is my age 22').toUpperCase();
+    if(myAge === 'Y' || myAge === 'YES'){
         alert('Good job you guessed right!');
         count++;
-    } else if(myAge === 'N'){
+    } else if(myAge === 'N' || myAge === "NO"){
         alert('Wrong Answer Sorry');
     }else{
             alert('Sorry but you didnt follow my instructions please only answer with\
              Y and N.');
-    }
+    } console.log(myAge);
     //Do i love tequila question
-    var myLoveForTequila = prompt('Do i love tequila?');
-    myLoveForTequila === myLoveForTequila.toUpperCase;
-    console.log('Do i love tequila?');
-    if(myLoveForTequila === 'Y'){
+    var myLoveForTequila = prompt('Do i love tequila?').toUpperCase();
+    if(myLoveForTequila === 'Y'|| myLoveForTequila === 'YES'){
         alert("Of course who doesn't love tequila");
         count++;
-    }else if(myLoveForTequila === 'N'){
+    }else if(myLoveForTequila === 'N'|| myLoveForTequila === 'NO'){
         alert('Sorry to tell you but I do.');
     }else{
         alert('Sorry but you didnt follow my instructions please only answer with\
         Y and N.');
-    }
+    } console.log(myLoveForTequila);
     //Allergy question
-    var myAllergy = prompt('Am I allergic to anything?');
-    myAllergy === myAllergy.toUpperCase;
-    console.log('Am I allergic to anything?');
-    if(myAllergy === 'Y'){
+    var myAllergy = prompt('Am I allergic to anything?').toUpperCase();
+    
+    if(myAllergy === 'Y' || myAllergy === 'YES'){
         alert('Correct im allergic to mosquitos');
         count++;
-    } else if(myAllergy === 'N'){
+    } else if(myAllergy === 'N' || myJob === 'NO'){
         alert('Incorrect im allergic to mosquitos');
     } else{
         alert('Sorry but you didnt follow my instructions please only answer with\
              Y and N.');
-    }
+    } console.log(myAllergy);
     //Did i go to college question
-    var myEducation = prompt('Did i go to college?');
-    myEducation === myEducation.toUpperCase;
-    console.log('Did i go to college?');
-    if(myEducation === 'Y'){
+    var myEducation = prompt('Did i go to college?').toUpperCase();
+    if(myEducation === 'Y' || myEducation === 'YES'){
         alert('Correct but i dropped out.');
         count++;
-    } else if(myEducation === 'N'){
+    } else if(myEducation === 'N' || myJob === 'NO'){
         alert('Incorrect I did but i dropped out.');
     } else{
         alert('Sorry but you didnt follow my instructions please only answer with\
              Y and N.');
-     }
-    
-}
-alert('Congrats you got ' + count + ' out of 5 correct Congrats!');
-
-
-    
+     } console.log(myAge);
+alert('Congrats you got ' + count + ' out of 5 correct Congrats!');}
