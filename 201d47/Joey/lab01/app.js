f95203bb86bf0a6b51023a453cd7ed09067eecae
rkgallaway <rkgallaway@msn.com>
2019-04-02 23:18:15 -0700

    initial commit

---
+++ 201d47/Joey/lab01/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var username =prompt('do you like chocolate?');
+if (username === 'yes'){
+  alert('hi');
+} else {
+  alert ('thats too bad');
+}
+var oreos = prompt ('did you like oreos?');
+if (oreos === 'yes'){
+  alert ('you know oreos are chocolate yes?');
+}
+var then =prompt('if you like oreos you like chocolate then?');
+if (then === 'yes'){
+  alert ('then you must like chocolate');
+}
+var shame =prompt('please do not deny chocolate.');
+if (shame === 'no'){
+  alert ('how dare you deny chocolate! have at thee!');
+} var challenge =prompt('you cannot deny my challenge have at thee');
+if (challenge === 'no')
+  alert('get back here and fight!');
