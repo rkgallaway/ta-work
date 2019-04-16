5d30e42f370d85c09d13d5e6f8a72fd8fcd52d85
rkgallaway <rkgallaway@msn.com>
2019-04-16 09:20:36 -0700

    lab 11 solution

--- 201d47/Raven/lab9b/salmonCookies/js/app.js
+++ 201d47/Raven/lab9b/salmonCookies/js/app.js
@@ -92,8 +92,8 @@ function renderFooter() {
   var tdTotalFoot = document.createElement('td');
   tdTotalFoot.textContent = (grandTotal);
   trFoot.append(tdTotalFoot);
-  var salesFooter = document.getElementById('footer');
-  salesFooter.append(trFoot);
+  var referenceTable = document.getElementById('footer');
+  referenceTable.append(trFoot);
 }
 
 renderHeader();
